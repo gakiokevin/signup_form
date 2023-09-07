906c8a3599dc1b03adc1b6479167ec8c7a809872
kelvin Macharia <101465969+gakiokevin@users.noreply.github.com>
2023-09-07 15:33:13 +0300

    Update App.js

--- api/App.js
+++ api/App.js
@@ -13,7 +13,7 @@ app.use(cors({
     
 }))
 app.get('/',(req,res)=>{
-    res.json({message:'Hello'})
+    res.json('begin your magic')
 })
 //connecting to MongoDatabase   
 mongoose.connect(process.env.connection_string,{
